8de01ac0f81cb38fc713b9dfd7d19a3039bb7cd3
Jimin Lee <jimin.l@wustl.edu>
2024-04-23 21:48:49 +0000

    modify confusion matrix and balance class weights

--- stroke/src/stroke/project.py
+++ stroke/src/stroke/project.py
@@ -1,8 +1,8 @@
 import pandas as pd
-from kaggle.api.kaggle_api_extended import KaggleApi
+#from kaggle.api.kaggle_api_extended import KaggleApi
 import numpy as np
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score, ConfusionMatrixDisplay
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
 
@@ -43,11 +43,11 @@ def filter_adults(df) -> pd.DataFrame:
 
 def logistic_regression(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, stratify = y, random_state= 4)
-    model = LogisticRegression(solver='liblinear', random_state=0)
+    model = LogisticRegression(solver='liblinear', random_state=0, class_weight = 'balanced')
     model.fit(X_train,y_train)
     y_pred = model.predict(X_test)
     acc = model.score(X_test, y_test)
-    return y_test, y_pred, acc
+    return model, y_test, y_pred, acc
 
 def plot_cm(y_test, y_pred):
     cm = confusion_matrix(y_test, y_pred)
@@ -62,10 +62,8 @@ def plot_cm(y_test, y_pred):
             ax.text(j, i, cm[i, j], ha='center', va='center', color='red')
     plt.show()
 
-
 data = data()
 print(data)
-
 
 data = drop_na(data)
 data = make_binary(data)
@@ -73,8 +71,14 @@ data = remove_cols(data)
 data = filter_adults(data)
 data = data[data.gender != 2]
 X = data.drop(columns = ["stroke"])
-y_test, y_pred, acc = logistic_regression(X, data["stroke"])
+y = data["stroke"]
+model, y_test, y_pred, acc = logistic_regression(X, data["stroke"])
 print(acc)
+print(roc_auc_score(y, model.predict_proba(X)[:, 1]))
 
-plot_cm(y_test, y_pred)
+#plot_cm(y_test, y_pred)
 
+cm = confusion_matrix(y_test, y_pred, labels = model.classes_)
+disp = ConfusionMatrixDisplay(confusion_matrix = cm, display_labels = model.classes_)
+disp.plot()
+plt.savefig('confusion_matrix.png')
